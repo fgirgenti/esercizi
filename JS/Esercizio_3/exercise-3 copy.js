a39f2fd7b83e196cf23677b760ed54a034b5d3c5
Francesco Girgenti <girgenti65@gmail.com>
2022-01-18 17:50:19 +0100

    risposta alla domanda

--- JS/Esercizio_3/exercise-3 copy.js
+++ JS/Esercizio_3/exercise-3 copy.js
@@ -6,3 +6,6 @@ function addStudent(student) {
 
 addStudent('Marco');
 console.log(students);
+
+//possiamo aggiungere un elemento a students perché assegnarlo con "const" e quindi "readonly" vuol dire che non possiamo riassegnarlo, quindi se per esempio contiene l'oggetto o l'array students non possiamo assegnarli un altro array
+//ma possiamo comunque modificare il contenuto dell'oggetto / array non essendo una variabile, credo sia per questo
